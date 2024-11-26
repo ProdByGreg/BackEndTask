16e649de56174e0ce9e1a2e685f8f3934fa653e5
Aluno <ProdByGreg>
2024-11-25 21:16:09 -0300

    Att

---
+++ Task/Controllers/TasksController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Task.Models;
+
+namespace Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private static List<ModelTask> modelTasks =
+            new List<ModelTask>();
+
+
+
+
+
+
+        [HttpGet]
+        public ActionResult<List<ModelTask>> 
+        SearchTasks()
+        {
+            return Ok(modelTasks);
+        }
+
+
+
+
+
+
+        [HttpPost]
+        public ActionResult<List<ModelTask>>
+        AddTask(ModelTask newTask)
+        {
+            if (newTask.Description.Length < 10) 
+                return BadRequest("Adicione pelomenos 10 carateres!");
+
+                newTask.Id = modelTasks[modelTasks.Count - 1].Id + 1;
+
+            modelTasks.Add(newTask);
+            return Ok(modelTasks);
+        }
+
+
+
+
+
+
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<List<ModelTask>> 
+        DeleteTask(int id)
+        {
+            var taskDelete = modelTasks.FirstOrDefault(t => t.Id == id);
+
+            if (taskDelete == null)
+            {
+                return NotFound("Tarefa não encontrada");
+            }
+
+            modelTasks.Remove(taskDelete);
+
+            return Ok(modelTasks);
+        }
+    }
+}
